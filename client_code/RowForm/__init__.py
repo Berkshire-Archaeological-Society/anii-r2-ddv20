48c19379556ad82763b8ec935a341184bb141742
awpmbakker@gmail.com <awpmbakker@gmail.com>
2026-07-03 13:45:05 +0000

    Edited RowForm

--- client_code/RowForm/__init__.py
+++ client_code/RowForm/__init__.py
@@ -440,6 +440,7 @@ class RowForm(RowFormTemplate):
             col_name_txt = col[0][:-3] + "Txt"
             row_list[col_name_txt] = col[1]["field"].getText()
             row_list[col[0]] = col[1]["field"].get_html()
+            print(col_name_txt,row_list[col_name_txt])
           else:
             row_list[col[0]] = col[1]["field"].getText()
           #delta = col[1]["field"].getContents()
