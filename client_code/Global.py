9babdaf6f4f4bd00d20e3b29f894df21f9e4dc29
awpmbakker@gmail.com <awpmbakker@gmail.com>
2026-07-29 14:00:23 +0000

    Edited Global

--- client_code/Global.py
+++ client_code/Global.py
@@ -100,7 +100,7 @@ Quill_toolbarOptions = [
 ['clean']                                         # remove formatting button
 ]
 #
-# for every table we need an entry in this multi-level dictionary. There is one entry for all FS tables, so for these there is no need for a entry per fs table.
+# for every table we need an entry in this multi-level dictionary. There is one entry for all find specialists tables (fs tables), so for these there is no need for a entry per fs table.
 # 
 role_access = {
 'Site Viewer': {
